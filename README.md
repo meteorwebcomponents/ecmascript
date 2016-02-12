3c046d4c4346f3601e7f37ee0e9633e1f6ebe1b1
tkay <aruntknambiar@gmail.com>
2016-02-12 14:27:03 +0530

    as extension

---
+++ README.md
@@ -0,0 +1,7 @@
+# mwc:ecmascript
+
+An ecmascript compiler with ecma2015 preset for [mwc:compiler](https://github.com/meteorwebcomponents/compiler)
+
+### Usage
+
+`meteor add mwc:ecmascript` and you are good to go.
--- package.js
+++ package.js
@@ -1,17 +1,13 @@
 Package.describe({
   name: 'mwc:ecmascript',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
+  version: '0.0.3',
+  summary: 'es6 support for polymer.',
   git: 'https://github.com/meteorwebcomponents/ecmascript',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.use('underscore');
+  api.use('mwc:compiler@1.1.7');
   api.versionsFrom('1.0');
   api.addFiles(['ecmascript.js'],'server');
   api.export('MWCEcma','server');
@@ -21,9 +17,4 @@ Npm.depends({
   'babel-core':'6.5.1',
   'babel-preset-es2015':'6.5.0'
 });
-Package.onTest(function(api) {
-  api.use('underscore');
-  api.use('tinytest');
-  api.use('mwc:ecmascript');
-  api.addFiles('ecmascript-tests.js');
-});
+
